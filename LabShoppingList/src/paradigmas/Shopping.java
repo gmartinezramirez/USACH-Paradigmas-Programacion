c161ac17d56152e7e7d3df9d6c983f86bb1b8bf2
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2020-12-30 17:41:59 -0300

    feat(lab exercise): continue exercise

--- LabShoppingList/src/paradigmas/Shopping.java
+++ LabShoppingList/src/paradigmas/Shopping.java
@@ -21,10 +21,42 @@ public class Shopping {
     articles.add(article3);
 
     // Imprimir lista
+    printListOfArticles(articles);
+
+    // Remover objeto basado en su indice en la lista
+    int index = 0;
+    articles.remove(index);
+    printListOfArticles(articles);
+
+    // Remover objeto de la lista pasandole un objeto
+    articles.remove(article3);
+    printListOfArticles(articles);
+
+    String articleName = "article2";
+    Article foundArticle = getArticleByName(articles, articleName);
+    System.out.println("Articulo encontrado en la lista: ");
+    System.out.println(foundArticle.toString());
+
+    Article notFoundArticle = getArticleByName(articles, "hola");
+    System.out.println("Articulo encontrado en la lista: ");
+    if (notFoundArticle != null) {
+      System.out.println(notFoundArticle.toString());
+    }
+  }
+
+  private static Article getArticleByName(List<Article> aListOfArticles, String aName) {
+    for (Article item : aListOfArticles) {
+      if (item.getName().equals(aName)) {
+        return item;
+      }
+    }
+    return null;
+  }
+
+  private static void printListOfArticles(List<Article> articles) {
+    System.out.println("Lista");
     for (Article article : articles) {
       System.out.println(article.toString());
     }
-
-    System.out.println("Hello Shopping list");
   }
 }
