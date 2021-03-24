db6d3018be2d9a3ba0e9c37f0583e09d2b58348a
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2021-03-24 16:04:32 -0300

    feat(scheme project template): initial skeleton continues

---
+++ 2021-01/01-FP/scheme-project-template/RUT_main.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+;; Descripción: Esto es una descripción
+;; Dom: Lista lst, x integer 
+;; Rec: Lista lst, y integer
+;; Tipo de recursión: No aplica
+(define (extract str)
+  (substring str 4 7))
