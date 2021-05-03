c90c26eed1a37379c8abe94013f587be5c2caa03
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2021-05-02 22:38:55 -0400

    feat(functional programming): update according to classes

--- 2021-01/01-FP/03-high-order-functions/my-filter.rkt
+++ 2021-01/01-FP/03-high-order-functions/my-filter.rkt
@@ -1,28 +1,26 @@
 #lang racket
 
-;; Descripción: Constructor de un tipo de dato usuario
-;; Dom: id <int>, username <str>, password <str>, age <int>
-;; Rec: usuario <list>
-;; Tipo de recursión: NA
+
+;; Referencia: funcion vista en clase
 (define (my-filter pred lst)
   (cond
-    [(empty? lst) '()]
-    [(pred (car lst))
+    [(empty? lst) '()] ;null? null
+    [(pred (car lst))  ;; si predicado es verdero, ejecuto la siguiente linea, caso contrario, ejecuto else
      (cons (car lst) (my-filter pred (cdr lst)))]
     [else (my-filter pred (cdr lst))]))
 
-;; Descripción: Constructor de un tipo de dato usuario
-;; Dom: id <int>, username <str>, password <str>, age <int>
-;; Rec: usuario <list>
-;; Tipo de recursión: NA
+; (10 my-filter pred resto de la lista)  ; resto de la lista: 10 20 30 40 10 50 60 10
+; (10 10 my-filter pred resto d ela lista) ; resto de la lista: 20 30 40 10 50 60 10
+; (10 10 my-filter pred cdr lst) ; resto de la lista: 30 40  10 50 60 10
+; (10 10 my-filter pred cdr lst) ; resto de la lista: 40  10 50 60 10
+; (10 10 10 my-filter pred cdr lst) ; resto de la lista: 50 60 10
+
+;(my-filter (is-current-number-eq-to? 10) '(10 10 20 30 40 10 50 60 10))
+
 (my-filter
  even? ;; pred
  '(1 2 3 4 5)) ;; lst
 
-;; Descripción: Constructor de un tipo de dato usuario
-;; Dom: id <int>, username <str>, password <str>, age <int>
-;; Rec: usuario <list>
-;; Tipo de recursión: NA
 (my-filter
  (negate even?) ;; pred
  '(1 2 3 4 5)) ;; lst
@@ -43,12 +41,33 @@
              #t]
             [else #f]))))
 
+;; Funcion predicado
 (define is-current-number-eq-to?
   (lambda (number-to-compare)
     (lambda (current-number)
       (cond [(eq? current-number number-to-compare) #t] ; Se puede utilizar eq? o =
             [else #f]))))
 
-(my-filter (is-current-number-eq-or-greater-than? 20) '(10 20 30 40 50))
-(my-filter (is-current-number-eq-or-less-than? 20) '(10 20 30 40 50))
-(my-filter (is-current-number-eq-to? 20) '(10 20 30 40 50))
+;; El objetivo es filtrar de la lista, los elementos que sean mayores o iguales a N
+(my-filter (is-current-number-eq-or-greater-than? 30) '(10 20 30 40 50))
+
+
+
+;(my-filter (is-current-number-eq-or-less-than? 20) '(10 20 30 40 50))
+
+;; El objetivo es filtrar de la lista, los elementos que sean iguales a N
+;(my-filter (is-current-number-eq-to? 10) '(10 10 20 30 40 10 50 60 10))
+
+
+;; Insertar un elemento en una lista sin duplicados
+
+;(define is-the-element-in-current-list?
+
+;(define insertar
+;  (lambda (elemento lista)
+;    (cons elemento (my-filter
+;                    (lambda (e)
+;                      (not (eqv? elemento e)))
+;                    lista))))
+
+;(insertar 1 '(10 20 30 40 50 1 2))
