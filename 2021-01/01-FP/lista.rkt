58a6b00c9198036d5288dc4c6b1175ce87bd163c
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2021-05-02 22:38:04 -0400

    feat(functional programming): update according to classes

---
+++ 2021-01/01-FP/lista.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(provide obtener-lista-con-datos)
+(provide obtener-lista2-con-datos)
+
+
+;.rkt
+;LISP, LISt processor. Lost in a sea of parenteshis.
+
+(define obtener-lista-con-datos
+  (list "hola" "chao"))
+
+(define obtener-lista2-con-datos
+  (list "hola2" "chao2"))
---
+++ 2021-01/01-FP/main.rkt
@@ -0,0 +1,7 @@
+#lang racket
+
+(require "lista.rkt")
+
+obtener-lista-con-datos
+
+obtener-lista2-con-datos
