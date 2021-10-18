bdf5b1654431812645489c7d44ca1fc6568252ec
Gonzalo Martinez <gonzalo.martinez@cornershopapp.com>
2021-10-17 22:18:42 -0300

    Actualizacion comentario operaciones TDA

--- 2021-02/01-FP/ejemplo-lista-compras/tda/operaciones-tda.rkt
+++ 2021-02/01-FP/ejemplo-lista-compras/tda/operaciones-tda.rkt
@@ -2,6 +2,18 @@
 
 (provide if-element-is-number-then->string)
 
+; TDA Operaciones TDA ;
+
+; Capa otras funciones
+
+;; Descripcion: Funcion que verifica si un elemento es un numero, si lo es lo transforma a string
+;; Dom: number | string
+;; Rec: string
+;; Ejemplo:
+;; - Entrada: (if-element-is-number-then->string 815)
+;; - Salida: "815"
+;; - Entrada: (if-element-is-number-then->string "hola como estas")
+;; - Salida: "hola como estas"
 (define if-element-is-number-then->string
   (lambda (element)
     (cond
