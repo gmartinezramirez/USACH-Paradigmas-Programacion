506e2da25a9dc10498221b237dca0cbe52938520
Gonzalo Martinez <gonzalo.martinez@cornershopapp.com>
2021-12-05 15:11:48 -0300

    add prolog products example

---
+++ 2021-02/02-Prolog/productos.pl
@@ -0,0 +1,7 @@
+% Facts
+
+producto_precio(playstation5, 700000).
+producto_precio(tomate, 900). 
+producto_precio(completo, 1000). 
+producto_precio(sopaipilla, 1000).
+producto_precio(hamburguesa 1000).
