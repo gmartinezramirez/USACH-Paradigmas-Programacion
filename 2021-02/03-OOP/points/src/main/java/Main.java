c4ee6a475f794dcc63aafc0b5efb55b6ded27133
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2021-12-26 00:02:29 -0300

    Update

--- 2021-02/03-OOP/points/src/main/java/Main.java
+++ 2021-02/03-OOP/points/src/main/java/Main.java
@@ -1,10 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        Point p1 = new Point(3, 2);
-        Point p2 = new Point(5, 8);
+        Point p1 = new Point(3, 4);
+        Point p2 = new Point(6, 8);
 
         System.out.println(p1.toString());
         System.out.println(p2.toString());
+        System.out.println(p1.distance(p2));
     }
 }
