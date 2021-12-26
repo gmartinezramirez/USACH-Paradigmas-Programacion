2ed85808ddf69d96f9a236f485129324656f0363
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2021-12-25 21:13:18 -0300

    Add point OOP example

---
+++ 2021-02/03-OOP/points/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(3, 2);
+        Point p2 = new Point(5, 8);
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+    }
+}
